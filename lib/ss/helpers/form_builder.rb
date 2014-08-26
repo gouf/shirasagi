919cd66718e20984e54d8c88a16a6f8a81918e31
gouf <innocent.zero@gmail.com>
2014-08-27 04:42:19 +0900

    refactor: set indent level as public/private method

--- lib/ss/helpers/form_builder.rb
+++ lib/ss/helpers/form_builder.rb
@@ -2,38 +2,37 @@
 module SS::Helpers
   class FormBuilder < ActionView::Helpers::FormBuilder
     public
-      def hidden_field(method, options = {})
-        return super if method !~ /\[/
-          
-        object_method = "#{@object_name}[" + method.sub("[", "][")
-        value = options[:value] || array_value(method)
-        options.delete(:value)
-        
-        if !value.is_a?(Array) || value.size == 0
-          return @template.hidden_field_tag(object_method, value, options)
-        end
-        
-        tags = value.map do |v|
-          options[:id] ||= object_method.gsub(/\W+/, "_") + "#{v}"
-          @template.hidden_field_tag(object_method, v, options)
-        end
-        tags.join.html_safe
+
+    def hidden_field(method, options = {})
+      return super if method !~ /\[/
+
+      value = options[:value] || array_value(method)
+      options.delete(:value)
+
+      if !value.is_a?(Array) || value.size == 0
+        return @template.hidden_field_tag(object_method, value, options)
       end
-      
-      def check_box(method, options = {}, checked_value = "1", unchecked_value = "0")
-        return super if method !~ /\[/
-        
-        object_method = "#{@object_name}[" + method.sub("[", "][")
-        if method =~ /\[\]$/
-          checked = array_value(method).include?(checked_value)
-          options[:id] ||= object_method.gsub(/\W+/, "_") + "#{checked_value}"
-        else
-          checked = array_value(method).present?
-        end
-        
-        @template.check_box_tag(object_method, checked_value, checked, options)
+
+      tags = value.map do |v|
+        options[:id] ||= object_method.gsub(/\W+/, '_') + "#{v}"
+        @template.hidden_field_tag(object_method, v, options)
       end
-    
+      tags.join.html_safe
+    end
+
+    def check_box(method, options = {}, checked_value = '1', unchecked_value = '0')
+      return super if method !~ /\[/
+
+      if method =~ /\[\]$/
+        checked = array_value(method).include?(checked_value)
+        options[:id] ||= object_method.gsub(/\W+/, '_') + "#{checked_value}"
+      else
+        checked = array_value(method).present?
+      end
+
+      @template.check_box_tag(object_method, checked_value, checked, options)
+    end
+
     private
 
     def array_value(method)
@@ -46,6 +45,9 @@ module SS::Helpers
         eval("item.#{code}")
       end
     end
-      end
+
+    def object_method
+      "#{@object_name}[" + method.sub('[', '][')
+    end
   end
 end
